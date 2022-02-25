b2349be43c3c405489f1d8f2b7cab9a89f550db7
Harris Sultan <harris_sultan@outlook.com>
2022-02-24 19:30:31 -0500

    commit tree 376ce99c2987e82f2d16def04de48f2fb38774ad

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    while 0 < num
+      puts "#{num} SECOND(S)!"
+      num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(num)
+    sleep(5)
+    while 0 < num
+      puts "#{num} SECOND(S)!"
+      num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
